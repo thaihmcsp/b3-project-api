c468859e96998d150f095fffa1658bb4f3c6a6ad
thaidd7b <thaidd7b@gmail.com>
2022-09-06 14:40:22 +0700

    add product API

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,24 @@
 const Product = require("../models/Product");
 const ProductDetail = require("../models/ProductDetail");
+const fs = require('fs')
+const path = require('path');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './publics/uploads/products');
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname);
+      cb(null, file.fieldname + '-' + uniqueSuffix);
+    }
+})
+
+exports.upload = multer({ storage: storage , 
+    fileFilter: function(req, file, cb){
+        if(!file.mimetype.includes('image')) return cb(new Error('I don\'t have a clue!'))
+
+        cb(null, true)
+}})
 
 exports.getAllProducts = async (req, res) => {
     try {
@@ -24,6 +43,39 @@ exports.getOneProduct = async (req, res) => {
     try {
         const product = await Product.findById(req.params.productId).populate('listDtail.productDetails').populate('categoryId');
         res.status(200).json({product});
+    } catch (error) {
+        res.status(500).json({message: 'server error', error});
+    }
+}
+
+exports.updateProductThumb = async (req, res) => {
+    try {
+        let product = await Product.findOne({_id: req.params.productId});
+        if(!req.file) return res.status(400).json({message: 'please choose an image'});
+        if(!product.thumbnail.startsWith('http')){
+            fs.unlink(product.thumbnail, () => {return});
+        }
+        let newproduct = await Product.findOneAndUpdate({_id: req.params.productId}, {thumbnail: req.file.path}, {new: true, runValidators: true});
+        res.status(200).json({product: newproduct});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'server error', error});
+    }
+}
+
+exports.updateProductInfo = async (req, res) => {
+    try {
+        const product = await Product.findOneAndUpdate({_id: req.params.productId}, req.body, {new: true, runValidators: true});
+        res.status(200).json({product});    
+    } catch (error) {
+        res.status(500).json({message: 'server error', error});
+    }
+}
+
+exports.findProductByName = async (req, res) => {
+    try {
+        const products = await Product.find({productName: {$regex: req.query.productName, $options: 'i'}}).populate('listDtail.productDetails').populate('categoryId');
+        res.status(200).json({products});    
     } catch (error) {
         res.status(500).json({message: 'server error', error});
     }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,12 @@
-const { getAllProducts, createProduct, getOneProduct } = require('../controllers/productController');
+const { getAllProducts, createProduct, getOneProduct, upload, updateProductThumb, updateProductInfo, findProductByName } = require('../controllers/productController');
 const { checkLogin, checkAdmin } = require('../middlewares/auth');
 const router = require('express').Router();
 
 router.get('/get-all-products', getAllProducts);
+router.get('/find-products-by-name', findProductByName);
 router.get('/get-one-product/:productId', getOneProduct);
 router.post('/create-product', checkLogin, checkAdmin, createProduct);
+router.patch('/update-product-thumb/:productId', checkLogin, checkAdmin, upload.single('thumb'), updateProductThumb);
+router.patch('/update-product-info/:productId', checkLogin, checkAdmin, updateProductInfo);
 
 module.exports = router;
